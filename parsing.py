b06856ee3516a9a1a70063317e81fb8de0aa24be
Nastya <60959353+WaterMemor@users.noreply.github.com>
2020-12-13 17:33:11 +0300

    Add files via upload

---
+++ parsing.py
@@ -0,0 +1,41 @@
+import requests
+from pandas import read_excel
+from bs4 import BeautifulSoup
+import locale
+import shutil
+import os.path
+# myLocale=locale.setlocale(category=locale.LC_ALL, locale="ru_RU.UTF-8")
+dir='work\\picN\\'
+file=dir+'Example.xlsx'
+df = read_excel(file, sheet_name = '1')
+foto=df['Foto'].tolist()
+art=df['Art'].tolist()
+
+index=1
+for f in foto:
+    url=str(f)
+    print('url ',url)
+    if url.find('http')<0: continue
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,'html.parser')
+    images=soup.find_all('img') 
+    iter=0
+    for i in images:
+        src=i.get('src')
+        if src.find('upload')>0:
+            art1= str(art[index-1])
+            art1='sh'+art1.replace('.0','')
+        
+            source='http://photo.mirigr.by'+src
+            destination=dir+str(art1)+'.jpg'
+    
+            print('source,', destination, ',destination')
+            if not os.path.isfile(destination) :
+                filereq = requests.get(source,stream = True)
+                with open(destination,"wb") as receive:
+                    shutil.copyfileobj(filereq.raw,receive)
+                    del filereq
+                iter=iter+1   
+            break  #break for 1 foto           
+        # print ('tables',tables)
+    index=index+1
